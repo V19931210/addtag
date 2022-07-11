71a32e2ef35a7da29fd0191b38444d6d125090a5
qiquanwei <qiquanwei@shizhuang-inc.com>
2022-07-11 14:03:27 +0800

    first cm

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main() {
+	r := gin.Default()
+
+	// 测试路由
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// 启动服务器
+	r.Run(":1210")
 }
